35b7fe825d13f9e623be888faf460efa29270fc1
Jan Wambach <jan@blaxxer.de>
2020-02-27 11:48:47 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ echo "Zip file created."
 
 echo "Deploying files"
 
-sshpass -p $DEPLOY_PASSWORD scp -o StrictHostKeyChecking=no brainbox-theme-latest.zip ${DEPLOY_USERNAME}@${TARGET_SERVER}:${M_TMP_DIR}
+sshpass -p $DEPLOY_PASSWORD scp -o StrictHostKeyChecking=no brainbox-theme-latest.zip ${DEPLOY_USERNAME}@${TARGET_SERVER}:${M_REMOTE_DIR}
 
 sshpass -p $DEPLOY_PASSWORD ssh ${DEPLOY_USERNAME}@${TARGET_SERVER} bash -c "'
 
