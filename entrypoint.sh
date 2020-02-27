dc4313a122cf14f05f619bf7494575086b750db6
Jan Wambach <jan@blaxxer.de>
2020-02-27 10:14:32 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ sshpass -p $DEPLOY_PASSWORD scp -o StrictHostKeyChecking=no brainbox-theme-lates
 sshpass -p $DEPLOY_PASSWORD ssh ${DEPLOY_USERNAME}@${TARGET_SERVER} bash -c "'
 
 cd ${M_REMOTE_DIR}
-cp ${LOCAL_DIR:-"./"}/brainbox-theme-latest.zip ${REMOTE_DIR:-"~/"}
+cp ${LOCAL_DIR:-"./"}brainbox-theme-latest.zip ${REMOTE_DIR:-"~/"}
 
 ${EXTRA_COMMANDS}
 '"
