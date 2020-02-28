b9f68200299e063fb538b11e082eb320ef0d6462
Jan Wambach <jan@blaxxer.de>
2020-02-28 09:27:21 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,26 @@
 #!/bin/sh -l
 
-M_LOCAL_DIR=${LOCAL_DIR:-"."}
+M_LOCAL_DIR=${LOCAL_DIR:-"./"}
 M_REMOTE_DIR=${REMOTE_DIR:-"~/"}
 
 echo "Creating a zip files..."
+
 cd $M_LOCAL_DIR
+
+echo "Creating wrapper directory..."
+
+mkdir .brainbox-theme
+mv * .brainbox-theme/
+mv .brainbox-theme brainbox-theme
+
+echo "Wrapper directory created..."
+
+echo "Moving to wrapper directory..."
+
+cd brainbox-theme
+
+echo "Zipping wrapper directory..."
+
 zip ~/brainbox-theme-latest.zip -r ./ -x "src/*" -x ".git/*" -x ".github/*" -x ".gitattributes" -x ".gitignore" -x "bower.json" -x "package.json" -x "webpack.config.js"
 cd ~/
 echo "Zip file created."
